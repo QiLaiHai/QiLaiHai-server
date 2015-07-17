c14ce70136a464ad0f2b0029deda2e5ccec9e2d0
Kata Hazi <cfdrws@users.noreply.github.com>
2015-07-17 18:36:42 +0800

    Create BaseInterceptor.java

---
+++ src/qilaihai/action/base/BaseInterceptor.java
@@ -0,0 +1,8 @@
+package qilaihai.action.base;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public abstract class BaseInterceptor extends AbstractInterceptor {
+	public static final String FAILED = "failed";
+}
